c584ccf67e8b450d95ea24bb3a7011e4f6670066
Tobey Hainge <tobeyhainge@gmail.com>
2023-02-10 06:13:19 -0500

    new

--- src/Hooks/email.js
+++ src/Hooks/email.js
@@ -1,5 +1,5 @@
-// const NODE_URL = 'https://simply-cyber.herokuapp.com/sendemail';
-const NODE_URL = 'http://localhost:3080/sendemail';
+const NODE_URL = 'https://simply-cyber.herokuapp.com/sendemail';
+// const NODE_URL = 'http://localhost:3080/sendemail';
 
 const sendEmail = async (data) => {
     return await fetch(`${NODE_URL}`, {
